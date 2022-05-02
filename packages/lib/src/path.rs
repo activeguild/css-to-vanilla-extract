919cc75bc142b2b35caaebc0cf6548f4c1107656
j1ngzoue <workdrive0306@gmail.com>
2022-05-03 01:12:58 +0900

    Refactor

--- packages/lib/src/path.rs
+++ packages/lib/src/path.rs
@@ -2,14 +2,13 @@ use std::{env, path::Path};
 
 type ResultPath = Result<String, std::ffi::OsString>;
 
-pub fn resolve_paths(path: String) -> Result<Vec<(ResultPath, ResultPath)>, bool> {
+pub fn resolve_paths(path: String) -> Result<Vec<(ResultPath, ResultPath)>, String> {
     let mut target = env::current_dir().unwrap();
     let mut _paths: Vec<(ResultPath, ResultPath)> = vec![];
 
     target.push(path);
     if !target.exists() {
-        println!("The specified path does not exist.{:?}\n", target);
-        return Err(false);
+        return Err(format!("The specified path does not exist.{:?}\n", target));
     }
 
     each_resolve_path(&mut _paths, &target)
@@ -18,27 +17,30 @@ pub fn resolve_paths(path: String) -> Result<Vec<(ResultPath, ResultPath)>, bool
 pub fn each_resolve_path(
     paths: &mut Vec<(ResultPath, ResultPath)>,
     target: &Path,
-) -> Result<Vec<(ResultPath, ResultPath)>, bool> {
+) -> Result<Vec<(ResultPath, ResultPath)>, String> {
     if target.is_dir() {
         let files = match target.read_dir() {
             Ok(value) => value,
-            Err(_) => return Err(false),
+            Err(error) => return Err(format!("{:?}", error)),
         };
         for dir_entry in files {
             let path = match dir_entry {
                 Ok(value) => value.path(),
-                Err(_) => continue,
+                Err(error) => {
+                    eprintln!("{:?}", &error);
+                    continue;
+                }
             };
 
             if path.is_dir() {
                 if each_resolve_path(paths, &path).is_err() {
-                    println!("Unable to resolve path :{:?}", &path);
+                    eprintln!("Unable to resolve path :{:?}", &path);
                 }
                 continue;
             }
 
             if resolve_path(&path, paths).is_err() {
-                println!("Unable to resolve path :{:?}", &path);
+                eprintln!("Unable to resolve path :{:?}", &path);
                 continue;
             }
         }
@@ -50,7 +52,7 @@ pub fn each_resolve_path(
 fn resolve_path(
     path: &Path,
     paths: &mut Vec<(ResultPath, ResultPath)>,
-) -> Result<Vec<(ResultPath, ResultPath)>, bool> {
+) -> Result<Vec<(ResultPath, ResultPath)>, String> {
     let mut output_path = path.to_path_buf();
     let mut output_file_name = String::new();
 
@@ -60,19 +62,24 @@ fn resolve_path(
             let formatted_file_name_as_str = &format!("{}.ts", file_name_as_str);
             output_file_name.push_str(formatted_file_name_as_str);
         } else {
-            return Err(false);
+            return Err(String::new());
         }
     } else {
-        return Err(false);
+        return Err(String::new());
     }
 
     output_path.pop();
     output_path.push(output_file_name);
     let output_path_as_str = output_path.into_os_string().into_string();
-    let path_as_str = path.to_path_buf().into_os_string().into_string();
-    if path_as_str.is_err() || output_path_as_str.is_err() {
-        return Err(false);
+    if let Err(error) = output_path_as_str {
+        return Err(format!("{:?}", error));
     }
+
+    let path_as_str = path.to_path_buf().into_os_string().into_string();
+    if let Err(error) = path_as_str {
+        return Err(format!("{:?}", error));
+    }
+
     paths.push((output_path_as_str, path_as_str));
 
     Ok(paths.clone())
