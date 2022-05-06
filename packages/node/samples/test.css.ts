2764b330db45d533d578b68803af0b3cf49a504b
j1ngzoue <workdrive0306@gmail.com>
2022-05-06 16:13:18 +0900

    Update samples

--- packages/node/samples/test.css.ts
+++ packages/node/samples/test.css.ts
@@ -1,116 +1,119 @@
-import {
-  globalFontFace,
-  globalKeyframes,
-  globalStyle,
-  style,
-} from "@vanilla-extract/css";
+import { globalStyle, globalKeyframes, globalFontFace, style } from "@vanilla-extract/css"
 
 globalFontFace("Open Sans", {
-  src: "url(/fonts/OpenSans-Regular-webfont.woff2) format(woff2) , url(/fonts/OpenSans-Regular-webfont.woff) format(woff)",
-});
+  src:"url(https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap)",
+},
+);
 globalKeyframes("slidein", {
-  from: {
-    transform: "translateX(0%)",
-  },
-  to: {
-    transform: "translateX(100%)",
-  },
-});
+"from": {
+  transform:"translateX(0%)",
+},
+"to": {
+  transform:"translateX(100%)",
+},
+},
+);
 globalStyle("*", {
-  boxSizing: "border-box",
-});
+  boxSizing:"border-box",
+},
+);
 globalStyle("*::after", {
-  boxSizing: "border-box",
-});
+  boxSizing:"border-box",
+},
+);
 globalStyle("*::before", {
-  boxSizing: "border-box",
-});
+  boxSizing:"border-box",
+},
+);
 globalStyle(":hover", {
-  color: "red",
-});
+  color:"red",
+},
+);
 globalStyle("[type^='button' s]", {
-  cursor: "pointer",
-});
+  cursor:"pointer",
+},
+);
 globalStyle("input", {
-  "@media": {
-    "(min-width: 1200px)": {
-      fontSize: "5rem",
-    },
-  },
-  "@supports": {
-    "(position:-webkit-sticky) or (position:sticky)": {
-      fontSize: "5rem",
-    },
-  },
-});
+"@media": {
+"(min-width: 1200px)": {
+  fontSize:"5rem",
+},
+},
+"@supports": {
+"(position:-webkit-sticky) or (position:sticky)": {
+  fontSize:"5rem",
+},
+},
+},
+);
 export const accordionButton = style({
-  "@media": {
-    "(min-width: 1200px)": {
-      selectors: {
-        "&:not": {
-          color: "0c63e4",
-        },
-        "&:not::after": {
-          transform: "rotate(-180deg)",
-        },
-      },
-    },
-  },
+"@media": {
+"(min-width: 1200px)": {
+"selectors": {
+"&:not": {
+  color:"0c63e4",
+},
+"&:not::after": {
+  transform:"rotate(-180deg)",
+},
+},
+},
+},
 });
 export const bar = style({
-  position: "absolute",
-  ":hover": {
-    boxSizing: "border-box",
-  },
+  position:"absolute",
+":hover": {
+  boxSizing:"border-box",
+},
 });
 export const display1 = style({
-  "@media": {
-    "(min-width: 1200px)": {
-      color: "red",
-      fontSize: "5rem",
-    },
-  },
+"@media": {
+"(min-width: 1200px)": {
+  color:"red",
+  fontSize:"5rem",
+},
+},
 });
 export const display2 = style({
-  backgroundColor: "blue",
-  "@media": {
-    "(min-width: 1200px)": {
-      color: "red",
-      fontSize: "5rem",
-    },
-  },
+  backgroundColor:"blue",
+"@media": {
+"(min-width: 1200px)": {
+  color:"red",
+  fontSize:"5rem",
+},
+},
 });
 export const display3 = style({
-  "@supports": {
-    "(position:-webkit-sticky) or (position:sticky)": {
-      fontSize: "5rem",
-    },
-  },
+"@supports": {
+"(position:-webkit-sticky) or (position:sticky)": {
+  fontSize:"5rem",
+},
+},
 });
 export const display4 = style({
-  "@supports": {
-    "(position:-webkit-sticky) or (position:sticky)": {
-      color: "red",
-      fontSize: "5rem",
-    },
-  },
+"@supports": {
+"(position:-webkit-sticky) or (position:sticky)": {
+  color:"red",
+  fontSize:"5rem",
+},
+},
 });
 export const display5 = style({
-  backgroundColor: "blue",
-  "@supports": {
-    "(position:-webkit-sticky) or (position:sticky)": {
-      color: "red",
-      fontSize: "5rem",
-    },
-  },
+  backgroundColor:"blue",
+"@supports": {
+"(position:-webkit-sticky) or (position:sticky)": {
+  color:"red",
+  fontSize:"5rem",
+},
+},
 });
 export const foo = style({
-  position: "absolute",
+  position:"absolute",
 });
 export const toast = style({
-  selectors: {
-    "&:not(:last-child)": {
-      marginBottom: "0.75rem",
-    },
-  },
+"selectors": {
+"&:not(:last-child)": {
+  marginBottom:"0.75rem",
+},
+},
 });
