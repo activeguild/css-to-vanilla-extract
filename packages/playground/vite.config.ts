8d7e70ebc2e11ba023c4618e14895cb1d315deef
j1ngzoue <workdrive0306@gmail.com>
2022-05-26 16:27:51 +0900

    Add analyze

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import sassDts from "vite-plugin-sass-dts";
-
+import { visualizer } from "rollup-plugin-visualizer";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), sassDts()],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react(),
+      sassDts(),
+      mode === "analyze" && visualizer({ gzipSize: true, open: true }),
+    ],
+  };
 });
