06fd1a98e2a11dcc19ef87452051fffe04139f15
j1ngzoue <workdrive0306@gmail.com>
2022-05-27 11:10:21 +0900

    Update font color on prism

--- packages/playground/src/prismjs/prismjs.ts
+++ packages/playground/src/prismjs/prismjs.ts
@@ -3,7 +3,7 @@ import "prismjs/components/prism-javascript.min.js";
 import "prismjs/components/prism-typescript.min.js";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.min.js";
-import "./prism-toolbar.css";
 import "prismjs/plugins/toolbar/prism-toolbar.min.js";
 import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js";
 import "prismjs/themes/prism-okaidia.css";
+import "./prism.css";
